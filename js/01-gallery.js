878dff70a6712d689b5d57418de72e4d47536d52
Dmytro Korolik <dimytry.korolik@gmail.com>
2022-08-01 21:24:05 +0300

    1st task is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,64 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarcup = createGalletyItemMarcup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarcup);
+
+galleryContainer.addEventListener('click', onGalleryImageClick);
+
+function createGalletyItemMarcup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </div>
+    `;
+    })
+    .join('');
+}
+
+function onGalleryImageClick(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const bigImage = e.target.dataset.source;
+
+  openImage(bigImage);
+}
+
+function openImage(image) {
+  function onEscPress(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  }
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${image}">
+`,
+    {
+      onShow: instance => {
+        document.addEventListener('keydown', onEscPress);
+      },
+      onClose: instance => {
+        document.removeEventListener('keydown', onEscPress);
+      },
+    }
+  );
+
+  instance.show();
+}
