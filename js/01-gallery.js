75fa595f63c6fc6212ee31a39ef4b4e89ab0116f
Dmytro Korolik <dimytry.korolik@gmail.com>
2022-08-01 22:49:00 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,25 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
-const galleryMarcup = createGalletyItemMarcup(galleryItems);
+const galleryMarcup = createGalletyItemsMarcup(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarcup);
 
 galleryContainer.addEventListener('click', onGalleryImageClick);
 
-function createGalletyItemMarcup(galleryItems) {
+function createGalletyItemsMarcup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
     <div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-        <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-        />
-    </a>
+      <a class="gallery__link" href="${original}">
+          <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+          />
+      </a>
     </div>
     `;
     })
